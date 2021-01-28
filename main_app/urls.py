0bc4348524ffc883ac97a9e0fd0cb9a808fd0cc9
erindoman <erin.doman@gmail.com>
2021-01-28 16:53:07 -0600

    fix name issue

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('items/', views.items_index, name='index'),
-    path('items/<int:item_id>/', views.items_detail, name='item')
+    path('items/<int:item_id>/', views.items_detail, name='detail')
 ]
